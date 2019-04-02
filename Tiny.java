f087af6b1afb46cc7b5db8dae41c1e78b4268568
Puraneshi <41456546+Puraneshi@users.noreply.github.com>
2019-04-02 15:44:52 -0300

    Add files via upload

---
+++ Tiny.java
@@ -0,0 +1,19 @@
+
+import java.io.*;
+import java.util.*;
+import lp.*;
+
+@SuppressWarnings("unused")
+public class Tiny {
+
+    public static void main(String[] args) {
+
+        Interpretador inter = new Interpretador(args[0]);
+        if (args.length == 1) {
+            inter.leArquivo();
+            inter.executa();
+        } else if ((args.length == 2) && (args[1].equals("-l"))) {
+            inter.listaArquivo();
+        }
+    }
+}
